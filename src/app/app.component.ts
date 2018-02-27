5382231a79dece26e441d4887166fd02ece0cb21
Matt Collinge <mattycov@googlemail.com>
2018-02-27 17:28:02 +0000

    removed redundant template files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,25 @@ import { GetMonsters } from "./store";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  template: `
+  <section class="hero is-light">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          MHW Reference
+        </h1>
+        <h2 class="subtitle">
+          MHW weakness and classification quick reference
+        </h2>
+      </div>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <app-monster-list></app-monster-list>
+    </div>
+  </section>
+  `
 })
 export class AppComponent implements OnInit {
   title = "app";
